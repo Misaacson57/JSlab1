48ab98ee491b48197ae3d9dbdcb3e756e8905264
Michael Isaacson <misaacson0507@gmail.com>
2026-07-15 21:28:18 -0400

    made my own calculate total function

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -6,12 +6,24 @@ function calculateArea (){
 // .value: After accessing the HTML element, .value is used to retrieve the value entered into the input field associated with that element.
 //  For instance, if a user enters '5' into the input field for length, .value retrieves the string '5'.
 
-    width = parseFloat(document.getElementById('widht').value);
+    width = parseFloat(document.getElementById('width').value);
 
     let area = width * length;
 
-    document.getElementsById('result').innerText = `The area of the rectangle is ${area}`;
+    document.getElementById('result').innerText = `The area of the rectangle is ${area}`;
     // the first part brings back the result id from the html file which is associated with the <p> tag
     // the .innerText is what i use to alter the text content in the HTML file
+}
+
+let amount;
+let price;
+
+function calculateTotal () {
+    amount = parseFloat(document.getElementById('amount').value);
+    price = parseFloat(document.getElementById('price').value);
+
+    let total = price * amount;
+
+    document.getElementById('totalPrice').innerText = `The total price is going to be: ${total}`;
 
 }
