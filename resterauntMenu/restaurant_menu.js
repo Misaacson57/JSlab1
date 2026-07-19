89f1e5cba699e1ff3741fb0aa0e8ac35c969c2be
Michael Isaacson <misaacson0507@gmail.com>
2026-07-19 12:46:06 -0400

    adding breakfast menu prices with different methods

--- resterauntMenu/restaurant_menu.js
+++ resterauntMenu/restaurant_menu.js
@@ -1,8 +1,19 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+const breakfastMenu = ['Pancakes- $12', 'Eggs Benedict -$22.99', 'Oatmeal -$21.99', 'Frittata -$15'];
 const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
 const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
 
-const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+
+/*const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+    document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+let breakfastMenuItemsHTML = '';
+breakfastMenu.forEach((item, index) => {
+    breakfastMenuItemsHTML += `<p>Item for each mode ${index +1}: ${item}</p>`;
+});
+document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+*/
+let breakfastMenuItemsHTML ='';
+for (i =0; i < breakfastMenu.length; i++){
+    breakfastMenuItemsHTML += `<p>Item For Loop ${i + 1}: ${breakfastMenu[i]}</p>`;}
     document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
 
 let mainCourseItem = '';
